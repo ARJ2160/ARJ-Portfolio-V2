7e32736cab3fc71b999cc07b87dfe1c855d9903b
Atharva <atharvaj2160@gmail.com>
2022-08-06 23:35:46 +0530

    Added Meta Tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import 'aos/dist/aos.css';
 import Aos from 'aos';
 import type { AppProps } from 'next/app';
 import React, { useEffect } from 'react';
+import Head from 'next/head';
+import Layout from './partials/Layout';
 
 /**
  * This is the Home Page
@@ -15,7 +17,36 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       once: true
     });
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta property='og:url' content='https://arj-portfolio-v2.vercel.app' />
+        <meta property='og:type' content='website' />;
+        <meta property='og:site_name' content='ARJs Portfolio' />
+        <meta property='og:title' content='ARJs Portfolio' />
+        <meta itemProp='name' content='ARJs Portfolio' />
+        <meta name='author' content='ARJs Joshi' />
+        <meta name='robots' content='index, follow' />
+        <meta name='language' content='en-us' />
+        <meta charSet='utf-8' />
+        <meta
+          itemProp='image'
+          content='https://ik.imagekit.io/36athv2v82c8/Screenshot-2022-08-03-134638_DrR3x5ppE.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1659534452367'
+        />
+        <meta
+          property='og:image'
+          content='https://ik.imagekit.io/36athv2v82c8/Screenshot-2022-08-03-134638_DrR3x5ppE.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1659534452367'
+        />
+        <meta
+          property='og:image:url'
+          content='https://ik.imagekit.io/36athv2v82c8/Screenshot-2022-08-03-134638_DrR3x5ppE.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1659534452367'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
---
+++ pages/partials/Layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Layout = ({ children }: any) => {
+  return <main>{children}</main>;
+};
+
+export default Layout;
