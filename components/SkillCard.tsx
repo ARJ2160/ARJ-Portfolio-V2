85c77121ec76d551cf271b0281bd4622445893e6
Atharva <atharvaj2160@gmail.com>
2023-12-01 09:00:01 +0530

    update skillcard

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -12,15 +12,14 @@ export const SkillCard = ({
       className='w-40 flex justify-center items-center flex-col mx-auto text-center p-4 rounded-xl border-2 bg-[#CCD6F6] text-black border-gray-300'
       key={id}
     >
-      <span className='w-16 h-14'>
-        <Image
-          width={skillIcon?.width}
-          height={skillIcon?.height}
-          src={skillIcon}
-          alt=''
-        />
-      </span>
-      <p className='text-xl mt-7'>{skillName}</p>
+      <Image
+        width={skillIcon?.width}
+        height={skillIcon?.height}
+        src={skillIcon}
+        className='w-16 h-16'
+        alt=''
+      />
+      <div className='text-xl mt-5'>{skillName}</div>
     </div>
   );
 };
